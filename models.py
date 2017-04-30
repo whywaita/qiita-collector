80b7476cf1dc07ee007f0ef6a282175ba6ea8677
whywaita <whywaita@whywrite.it>
2017-04-30 12:52:07 +0900

    add favorites table

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy.orm
 from werkzeug.security import generate_password_hash, \
              check_password_hash
 
@@ -7,10 +8,11 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
-    __tablename__ = "user"
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    pwdhash = db.Column(db.String(1000000))
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    pwdhash = db.Column(db.String(1000000))  # TODO: 後で適切な値にする
+    admin = db.Column(db.Boolean, default=0)
 
     def __init__(self, name, password):
         self.name = name
@@ -24,3 +26,24 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.pwdhash, password)
+
+
+class Favorite(db.Model):
+    __tablename__ = "favorites"
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.Text)
+    users_id = db.Column(
+            db.Integer,
+            db.ForeignKey('users.id')
+            )
+
+    user = sqlalchemy.orm.relationship(
+            "User",
+            backref=sqlalchemy.orm.backref(
+                'favorites',
+                order_by=id
+                )
+            )
+
+    def __repr__(self):
+        return '<Favorite %r>' % self.id
